c41026aed924ac27ade21558c3bb51e82ab7274f
Nancy <wainainanancywambui@gmail.com>
2024-09-21 17:32:47 +0300

    modigied page.tsx

--- investika-dashboard/src/app/page.tsx
+++ investika-dashboard/src/app/page.tsx
@@ -1,10 +1,8 @@
 import Layout from "./components/Layout";
-import HomePage from "./homepage/page";
 
 export default function Home() {
   return (
     <div>
-      <HomePage/>
       <Layout>
         <div>
         </div>
